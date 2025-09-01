9bdec53e5f287fb019d31ef31206a9fac0b60780
kota-shimizu <kohta.s.1118@gmail.com>
2025-09-01 15:49:10 +0900

    Revert "Add user input feature"

--- HelloGitApp/Program.cs
+++ HelloGitApp/Program.cs
@@ -1,7 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, Git + .NET!");
-
-Console.Write("あなたの名前を入力してください: ");
-string name = Console.ReadLine();   // ← キーボード入力を受け取る
-Console.WriteLine($"こんにちは、{name} さん！");
